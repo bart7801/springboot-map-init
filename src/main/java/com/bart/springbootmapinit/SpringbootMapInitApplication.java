99a7b28562fd059fbc6a2b5a6d1bf26fe6535219
bart7801 <bart7801@o2.pl>
2020-03-17 13:15:11 +0000

    Initial commit

---
+++ src/main/java/com/bart/springbootmapinit/SpringbootMapInitApplication.java
@@ -0,0 +1,13 @@
+package com.bart.springbootmapinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMapInitApplication.class, args);
+    }
+
+}
